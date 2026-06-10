a7d02e3867acd20235ce3c3484ab119ec205e45a
Nitendra <nitendramishra788@gmail.com>
2026-06-10 23:09:01 +0530

    Create variant Schema

---
+++ MegaMart-Backend/src/models/ProductVariant.js
@@ -0,0 +1,68 @@
+const mongoose = require("mongoose");
+const { seller } = require("../middlewares/sellerMiddleware");
+
+const productVariantSchema  = new mongoose.Schema({
+
+    product:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"product",
+        required:true,
+    },
+
+    seller:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true,
+
+    },
+
+    sku:{
+        type:String,
+        required:true,
+        unique:true,
+
+    },
+
+    color:{
+        type:String,
+        default:"",
+    },
+
+    size:{
+        type:String,
+        default:"",
+    },
+
+    price:{
+        type:Number,
+        required:true,
+
+    },
+
+    stock:{
+        type:Number,
+        required:true,
+    },
+
+    images:{
+        type:String,
+    },
+
+      isDefault: {
+      type: Boolean,
+      default: false,
+   },
+},
+
+{
+     timestamps: true,
+}
+
+);
+
+
+module.exports = mongoose.model(
+   "ProductVariant",
+   productVariantSchema
+);
+
