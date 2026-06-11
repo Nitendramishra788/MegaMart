3c474fd938dfbcd58877a133d9a1fd0b0e9ea132
Nitendra <nitendramishra788@gmail.com>
2026-06-11 23:05:29 +0530

    create category schema

---
+++ MegaMart-Backend/src/models/Category.js
@@ -0,0 +1,74 @@
+const mongoose = require("mongoose");
+ const  slugify = require("slugify");
+
+ const categorySchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        unique:true,
+        trim:true,
+    },
+
+    slug:{
+        type:String,
+        unique:true,
+    },
+
+    parentCategory:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"Category",
+        default:null,
+    },
+
+    level:{
+        type:Number,
+        default:0,
+    },
+
+    image:{
+        type:String,
+        default:"",
+    },
+
+    isActive:{
+        type:Boolean,
+        default:true,
+
+    },
+
+ },
+
+ {
+    timestamps:true
+ },
+
+
+);
+
+
+// generate auto slug
+
+categorySchema.pre("save", function (next) {
+
+  if (this.isModified("name")) {
+
+    this.slug = slugify(this.name, {
+      lower: true,
+      strict: true,
+    });
+
+  }
+
+  next();
+});
+
+
+// INDEXES
+categorySchema.index({ slug: 1 });
+
+categorySchema.index({
+  parentCategory: 1,
+});
+
+
+module.exports = mongoose.model("Category" , categorySchema);
