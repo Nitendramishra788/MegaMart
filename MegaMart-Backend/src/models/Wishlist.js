916dc3376ba31d1b7288073451abddb491cdc1e1
Nitendra <nitendramishra788@gmail.com>
2026-06-23 17:08:16 +0530

    feat(wishlist): create wishlist schema with user and variant references

---
+++ MegaMart-Backend/src/models/Wishlist.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const wishlistSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+
+  items: [
+    {
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "product",
+        required: true,
+      },
+
+      variant: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "ProductVariant",
+        required: true,
+      },
+
+      addedAt: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+});
+
+module.exports = mongoose.model(
+  "Wishlist",
+  wishlistSchema
+);
