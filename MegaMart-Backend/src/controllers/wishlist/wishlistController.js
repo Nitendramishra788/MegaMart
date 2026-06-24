d9b899d6113d9cd220e72c3c6d8e0223643840fc
Nitendra <nitendramishra788@gmail.com>
2026-06-24 12:58:31 +0530

    feat(wishlist): implement get wishlist with populate and formatted response

--- MegaMart-Backend/src/controllers/wishlist/wishlistController.js
+++ MegaMart-Backend/src/controllers/wishlist/wishlistController.js
@@ -5,62 +5,135 @@ const asyncHandler = require("../../utils/asyncHandler");
 const apiErrr = require("../../utils/apiErrr");
 
 const addWishlist = asyncHandler(async (req, res) => {
-  const { variantId } = req.body;
+    const { variantId } = req.body;
 
-  // validate variant
-  const variant = await Variant.findById(variantId);
+    // validate variant
+    const variant = await Variant.findById(variantId);
 
-  if (!variant) {
-    throw new apiErrr(404, "variant not found...!");
-  }
+    if (!variant) {
+        throw new apiErrr(404, "variant not found...!");
+    }
 
-  // find wishlist
-  let wishlist = await Wishlist.findOne({ user: req.user._id });
+    // find wishlist
+    let wishlist = await Wishlist.findOne({ user: req.user._id });
 
-  // IF wishlist NOT EXISTS → create
-  if (!wishlist) {
-    const newWishlist = await Wishlist.create({
-      user: req.user._id,
-      items: [
-        {
-          product: variant.product,
-          variant: variant._id,
-        },
-      ],
+    // IF wishlist NOT EXISTS → create
+    if (!wishlist) {
+        const newWishlist = await Wishlist.create({
+            user: req.user._id,
+            items: [
+                {
+                    product: variant.product,
+                    variant: variant._id,
+                },
+            ],
+        });
+
+        return res.status(200).json({
+            success: true,
+            message: "wishlist created successfully..!",
+            wishlist: newWishlist,
+        });
+    }
+
+    // check duplicate
+    const existWishlist = wishlist.items.find(
+        (item) =>
+            item.variant.toString() === variant._id.toString()
+    );
+
+    if (existWishlist) {
+        throw new apiErrr(409, "Item already in wishlist");
+    }
+
+    // push item
+    wishlist.items.push({
+        product: variant.product,
+        variant: variant._id,
     });
+
+    await wishlist.save();
 
     return res.status(200).json({
-      success: true,
-      message: "wishlist created successfully..!",
-      wishlist: newWishlist,
+        success: true,
+        message: "wishlist updated successfully..!",
+        wishlist,
     });
-  }
-
-  // check duplicate
-  const existWishlist = wishlist.items.find(
-    (item) =>
-      item.variant.toString() === variant._id.toString()
-  );
-
-  if (existWishlist) {
-    throw new apiErrr(409, "Item already in wishlist");
-  }
-
-  // push item
-  wishlist.items.push({
-    product: variant.product,
-    variant: variant._id,
-  });
-
-  await wishlist.save();
-
-  return res.status(200).json({
-    success: true,
-    message: "wishlist updated successfully..!",
-    wishlist,
-  });
 });
 
+
+// this is part of Get wishlist api...!
+const getWishlist = asyncHandler(
+  async (req, res) => {
+
+    const wishlist = await Wishlist.findOne({
+      user: req.user._id
+    })
+      .populate(
+        "items.product",
+        "title brand"
+      )
+      .populate(
+        "items.variant",
+        "price stock attributes"
+      );
+
+    if (!wishlist || wishlist.items.length === 0) {
+      return res.status(200).json({
+        success: true,
+        message: "your wishlist is empty please add something..!",
+        wishlist: {
+          items: [],
+          totalItems: 0,
+        }
+      });
+    }
+
+    // data formating
+
+    const formateItems = wishlist.items.map(
+
+      (item) => {
+
+        const product = item.product;
+        const variant = item.variant;
+
+        return {
+
+          _id: item._id,
+
+          product: {
+            _id: product?._id,
+            title: product?.title,
+            brand: product?.brand,
+          },
+
+          variant: {
+            _id: variant?._id,
+            price: variant?.price,
+            stock: variant?.stock,
+            attributes:
+              variant?.attributes,
+          },
+
+          addedAt: item.addedAt,
+        };
+      }
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "your all wishlist data",
+      wishlist: {
+        items: formateItems,
+        totalItems: formateItems.length,
+      }
+    });
+
+  }
+);
+
 module.exports = {
-  addWishlist,
+    addWishlist,
+    getWishlist,
 };
--- MegaMart-Backend/src/routers/wishlist/wishlistRouter.js
+++ MegaMart-Backend/src/routers/wishlist/wishlistRouter.js
@@ -4,13 +4,22 @@ const router = express.Router();
 
 const {
     addWishlist,
+    getWishlist,
 
 } = require("../../controllers/wishlist/wishlistController");
+const protect = require("../../middlewares/authMiddleware");
 
 router.post(
     "/add-wishlist",
     Protect,
     addWishlist,
-)
+);
+
+
+router.get(
+    "/get-wishlist",
+    protect,
+    getWishlist,
+);
 
 module.exports = router ;
