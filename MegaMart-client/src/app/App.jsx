2a9bd6c552d19c2e13e982bf5de06d598cc2102c
Nitendra <nitendramishra788@gmail.com>
2026-07-29 23:19:04 +0530

    chore: setup scalable frontend architecture

---
+++ MegaMart-client/src/app/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <main className="min-h-screen bg-slate-100 p-10">
+      <h1 className="text-4xl font-bold text-blue-600">
+        MegaMart Client 🚀
+      </h1>
+
+      <p className="mt-4 text-lg text-slate-700">
+        Customer frontend is running successfully.
+      </p>
+    </main>
+  );
+}
+
+export default App;
